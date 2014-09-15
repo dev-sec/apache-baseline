7fbe8b2e1084ebd04d189efb30c27e6fe78518e2
Edmund Haselwanter <me@ehaselwanter.com>
2014-09-15 10:14:35 +0200

    add limitexept test
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

--- default/serverspec/apache_spec.rb
+++ default/serverspec/apache_spec.rb
@@ -112,6 +112,7 @@ describe 'Apache Config' do
   describe 'should disable insecure HTTP-methods' do
     describe file(tmp_config) do
       its(:content) { should match(/^\s*?TraceEnable \s*?Off/) }
+      its(:content) { should match(/^\s*?<LimitExcept \s*?GET \s*?POST>/) }
     end
   end
 
