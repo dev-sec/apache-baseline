f74a77cfcd9f97b9629e38657d7430200cba0f45
Patrick Meier <patrick.meier111@googlemail.com>
2014-09-29 13:47:52 +0200

    fixing space issue in apache_spec.rb
    Signed-off-by: Patrick Meier <patrick.meier111@googlemail.com>

--- default/serverspec/apache_spec.rb
+++ default/serverspec/apache_spec.rb
@@ -89,7 +89,7 @@ describe 'Apache Config' do
   end
 
   # DTAG SEC: Req 3.36-12
-  pending  file(tmp_config) do
+  pending file(tmp_config) do
     its(:content) { should match(/^ServerTokens Prod/) }
   end
 
