d78ac7e85c7a105c88fb3a4001b1e767a033d68a
Edmund Haselwanter <me@ehaselwanter.com>
2014-11-02 07:21:12 +0100

    move combining files to spec test, so we can see if something goes wrong
    TelekomLabs-DCO-1.1-Signed-off-by: Edmund Haselwanter <me@ehaselwanter.com> (github: ehaselwanter)

--- default/serverspec/apache_spec.rb
+++ default/serverspec/apache_spec.rb
@@ -46,11 +46,16 @@ describe service("#{service_name}") do
   it { should be_running }
 end
 
-backend.run_command("cat #{apache_config} > #{tmp_config}; for i in `grep Include #{apache_config} | cut -d'\"' -f2`; do cat $i >> #{tmp_config}; done;")
+@max_servers = 0
 
-# max servers
-ret = backend.run_command("grep ServerLimit #{tmp_config} | tr -d [:alpha:][:space:]")
-max_servers = ret[:stdout].chomp.to_i
+# temporarily combine config-files and remove spaces
+describe 'Combining configfiles' do
+
+  describe command("cat #{apache_config} > #{tmp_config}; for i in `egrep '^\\s*Include' #{apache_config} | awk '{ print $2}'`; do [ $(ls -A $i) ]  && cat $i >> #{tmp_config} || echo no files in $i ; done;") do
+    its(:exit_status) { should eq 0 }
+  end
+
+end
 
 
 describe 'Apache Service' do
