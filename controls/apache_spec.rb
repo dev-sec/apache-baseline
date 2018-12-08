3cddce0581d22625def5418e40eb8e621aa33ce0
szEvEz <szivos.john@gmail.com>
2018-12-08 18:10:52 +0100

    fix control 1
    Signed-off-by: szEvEz <szivos.john@gmail.com>

--- controls/apache_spec.rb
+++ controls/apache_spec.rb
@@ -35,13 +35,13 @@ control 'apache-01' do
   desc 'Apache should be installed, running and automatically started at boot time'
   # https://www.inspec.io/docs/reference/resources/os/
   if os.debian?
-    describe service(apache2) do
+    describe service('apache2') do
       it { should be_installed }
       it { should be_running }
       it { should be_enabled }
     end
   elsif os.redhat?
-    describe service(httpd) do
+    describe service('httpd') do
       it { should be_installed }
       it { should be_running }
       it { should be_enabled }
@@ -68,49 +68,30 @@ control 'apache-03' do
   end
 end
 
-# control 'apache-04' do
-#   impact 1.0
-#   title 'Check Apache config folder owner, group and permissions.'
-#   desc 'The Apache config folder should owned and grouped by root, be writable, readable and executable by owner. It should be readable, executable by group and not readable, not writeable by others.'
-#   describe file(apache.conf_dir) do
-#     it { should be_owned_by 'root' }
-#     it { should be_grouped_into 'root' }
-#     it { should be_readable.by('owner') }
-#     it { should be_writable.by('owner') }
-#     it { should be_executable.by('owner') }
-#     it { should be_readable.by('group') }
-#     it { should_not be_writable.by('group') }
-#     it { should be_executable.by('group') }
-#     it { should_not be_readable.by('others') }
-#     it { should_not be_writable.by('others') }
-#     it { should_not be_executable.by('others') }
-#   end
-# end
-
-# control 'apache-05' do
-#   impact 1.0
-#   title 'Check Apache config file owner, group and permissions.'
-#   desc 'The Apache config file should owned and grouped by root, only be writable and readable by owner and not write- and readable by others.'
-#   describe file(apache.conf_path) do
-#     it { should be_owned_by 'root' }
-#     it { should be_grouped_into 'root' }
-#     it { should be_readable.by('owner') }
-#     it { should be_writable.by('owner') }
-#     it { should_not be_executable.by('owner') }
-#     it { should be_readable.by('group') }
-#     it { should_not be_writable.by('group') }
-#     it { should_not be_executable.by('group') }
-#     it { should_not be_readable.by('others') }
-#     it { should_not be_writable.by('others') }
-#     it { should_not be_executable.by('others') }
-#   end
-# end
+control 'apache-04' do
+  impact 1.0
+  title 'Check Apache config folder owner, group and permissions.'
+  desc 'The Apache config folder should owned and grouped by root, be writable, readable and executable by owner. It should be readable, executable by group and not readable, not writeable by others.'
+  describe file(apache.conf_dir) do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable.by('owner') }
+    it { should be_writable.by('owner') }
+    it { should be_executable.by('owner') }
+    it { should be_readable.by('group') }
+    it { should_not be_writable.by('group') }
+    it { should be_executable.by('group') }
+    it { should_not be_readable.by('others') }
+    it { should_not be_writable.by('others') }
+    it { should_not be_executable.by('others') }
+  end
+end
 
 control 'apache-05' do
   impact 1.0
   title 'Check Apache config file owner, group and permissions.'
   desc 'The Apache config file should owned and grouped by root, only be writable and readable by owner and not write- and readable by others.'
-  describe apache_conf do
+  describe file(apache.conf_path) do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_readable.by('owner') }
@@ -124,6 +105,25 @@ control 'apache-05' do
     it { should_not be_executable.by('others') }
   end
 end
+
+#control 'apache-05' do
+#  impact 1.0
+#  title 'Check Apache config file owner, group and permissions.'
+#  desc 'The Apache config file should owned and grouped by root, only be writable and readable by owner and not write- and readable by others.'
+#  describe apache_conf do
+#    it { should be_owned_by 'root' }
+#    it { should be_grouped_into 'root' }
+#    it { should be_readable.by('owner') }
+#    it { should be_writable.by('owner') }
+#    it { should_not be_executable.by('owner') }
+#    it { should be_readable.by('group') }
+#    it { should_not be_writable.by('group') }
+#    it { should_not be_executable.by('group') }
+#    it { should_not be_readable.by('others') }
+#    it { should_not be_writable.by('others') }
+#    it { should_not be_executable.by('others') }
+#  end
+#end
 
 control 'apache-06' do
   impact 1.0
