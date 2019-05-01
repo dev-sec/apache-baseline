bbf91d7e1ed2ec1c6906a1592a5cb9e1b2133b18
Sreeni Gadila <32882156+gadilasr@users.noreply.github.com>
2019-05-01 12:37:15 -0400

    Update apache_spec.rb
    changed from apache to httpd because of the httpd installed on CCS servers.
    
    Signed-off-by: Sreeni Gadila <sreenivas.gadila@gdit.com>

--- controls/apache_spec.rb
+++ controls/apache_spec.rb
@@ -22,7 +22,7 @@
 title 'Apache server config'
 
 only_if do
-  command(apache.service).exist?
+  command(httpd.service).exist?
 end
 
 title 'Apache server config'
@@ -31,7 +31,7 @@ control 'apache-01' do
   impact 1.0
   title 'Apache should be running'
   desc 'Apache should be running.'
-  describe service(apache.service) do
+  describe service(httpd.service) do
     it { should be_installed }
     it { should be_running }
   end
@@ -42,7 +42,7 @@ control 'apache-02' do
   title 'Apache should be enabled'
   desc 'Configure apache service to be automatically started at boot time'
   only_if { os[:family] != 'ubuntu' && os[:release] != '16.04' } || only_if { os[:family] != 'debian' && os[:release] != '8' }
-  describe service(apache.service) do
+  describe service(httpd.service) do
     it { should be_enabled }
   end
 end
@@ -50,7 +50,7 @@ end
 control 'apache-03' do
   title 'Apache should start max. 1 root-task'
   desc 'The Apache service in its own non-privileged account. If the web server process runs with administrative privileges, an attack who obtains control over the apache process may control the entire system.'
-  total_tasks = command("ps aux | grep #{apache.service} | grep -v grep | grep root | wc -l | tr -d [:space:]").stdout.to_i
+  total_tasks = command("ps aux | grep #{httpd.service} | grep -v grep | grep root | wc -l | tr -d [:space:]").stdout.to_i
   describe total_tasks do
     it { should eq 1 }
   end
@@ -60,7 +60,7 @@ control 'apache-04' do
   impact 1.0
   title 'Check Apache config folder owner, group and permissions.'
   desc 'The Apache config folder should owned and grouped by root, be writable, readable and executable by owner. It should be readable, executable by group and not readable, not writeable by others.'
-  describe file(apache.conf_dir) do
+  describe file(httpd.conf_dir) do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_readable.by('owner') }
@@ -79,7 +79,7 @@ control 'apache-05' do
   impact 1.0
   title 'Check Apache config file owner, group and permissions.'
   desc 'The Apache config file should owned and grouped by root, only be writable and readable by owner and not write- and readable by others.'
-  describe file(apache.conf_path) do
+  describe file(httpd.conf_path) do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_readable.by('owner') }
@@ -92,7 +92,7 @@ control 'apache-05' do
     it { should_not be_writable.by('others') }
     it { should_not be_executable.by('others') }
   end
-  describe file(File.join(apache.conf_dir, '/conf-enabled/hardening.conf')) do
+  describe file(File.join(httpd.conf_dir, '/conf-enabled/hardening.conf')) do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_readable.by('owner') }
@@ -112,8 +112,8 @@ control 'apache-06' do
   title 'User and group should be set properly'
   desc 'For security reasons it is recommended to run Apache in its own non-privileged account.'
   describe apache_conf do
-    its('User') { should eq [apache.user] }
-    its('Group') { should eq [apache.user] }
+    its('User') { should eq [httpd.user] }
+    its('Group') { should eq [httpd.user] }
   end
 end
 
@@ -122,7 +122,7 @@ control 'apache-07' do
   title 'Set the apache server token'
   desc '\'ServerTokens Prod\' tells Apache to return only Apache as product in the server response header on the every page request'
 
-  describe file(File.join(apache.conf_dir, '/conf-enabled/security.conf')) do
+  describe file(File.join(httpd.conf_dir, '/conf-enabled/security.conf')) do
     its('content') { should match(/^ServerTokens Prod/) }
   end
 
@@ -137,7 +137,7 @@ control 'apache-08' do
   title 'Should not load certain modules'
   desc 'Apache HTTP should not load legacy modules'
 
-  module_path = File.join(apache.conf_dir, '/mods-enabled/')
+  module_path = File.join(httpd.conf_dir, '/mods-enabled/')
   loaded_modules = command('ls ' << module_path).stdout.split.keep_if { |file_name| /.load/.match(file_name) }
 
   loaded_modules.each do |id|
@@ -182,7 +182,7 @@ control 'apache-10' do
   title 'Disable insecure HTTP-methods'
   desc 'Disable insecure HTTP-methods and allow only necessary methods.'
 
-  describe file(File.join(apache.conf_dir, '/conf-enabled/hardening.conf')) do
+  describe file(File.join(httpd.conf_dir, '/conf-enabled/hardening.conf')) do
     its('content') { should match(/^\s*?<LimitExcept\s+?GET\s+?POST>/) }
   end
 
@@ -197,7 +197,7 @@ control 'apache-11' do
   title 'Disable Apache\'s follows Symbolic Links for directories in alias.conf'
   desc 'Should include -FollowSymLinks or +SymLinksIfOwnerMatch for directories in alias.conf'
 
-  describe file(File.join(apache.conf_dir, '/mods-enabled/alias.conf')) do
+  describe file(File.join(httpd.conf_dir, '/mods-enabled/alias.conf')) do
     its('content') { should match(/-FollowSymLinks/).or match(/\+SymLinksIfOwnerMatch/) }
   end
 end
@@ -207,7 +207,7 @@ control 'apache-12' do
   title 'Disable Directory Listing for directories in alias.conf'
   desc 'Should include -Indexes for directories in alias.conf'
 
-  describe file(File.join(apache.conf_dir, '/mods-enabled/alias.conf')) do
+  describe file(File.join(httpd.conf_dir, '/mods-enabled/alias.conf')) do
     its('content') { should match(/-Indexes/) }
   end
 end
@@ -217,11 +217,11 @@ control 'apache-13' do
   title 'SSL honor cipher order'
   desc 'When choosing a cipher during an SSLv3 or TLSv1 handshake, normally the client\'s preference is used. If this directive is enabled, the server\'s preference will be used instead.'
 
-  describe file(File.join(apache.conf_dir, '/mods-enabled/ssl.conf')) do
+  describe file(File.join(httpd.conf_dir, '/mods-enabled/ssl.conf')) do
     its('content') { should match(/^\s*?SSLHonorCipherOrder\s+?On/i) }
   end
 
-  sites_enabled_path = File.join(apache.conf_dir, '/sites-enabled/')
+  sites_enabled_path = File.join(httpd.conf_dir, '/sites-enabled/')
   loaded_sites = command('ls ' << sites_enabled_path).stdout.split.keep_if { |file_name| /.conf/.match(file_name) }
 
   loaded_sites.each do |id|
@@ -239,7 +239,7 @@ control 'apache-14' do
   title 'Enable Apache Logging'
   desc 'Apache allows you to logging independently of your OS logging. It is wise to enable Apache logging, because it provides more information, such as the commands entered by users that have interacted with your Web server.'
 
-  sites_enabled_path = File.join(apache.conf_dir, '/sites-enabled/')
+  sites_enabled_path = File.join(httpd.conf_dir, '/sites-enabled/')
   loaded_sites = command('ls ' << sites_enabled_path).stdout.split.keep_if { |file_name| /.conf/.match(file_name) }
 
   loaded_sites.each do |id|
