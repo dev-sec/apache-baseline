e5a1aa4f413b5e3cb94b8cadf0e2f30b069443aa
Alex Pop <107378+alexpop@users.noreply.github.com>
2019-05-07 11:33:29 +0100

    Add apache library from core inspec as it was deprecated in inspec 4 (#30)
    * Add apache library from core inspec as it was deprecated in inspec 4
    
    Signed-off-by: Alex Pop <alexpop@users.noreply.github.com>
    
    * add frozen_string_literal
    
    Signed-off-by: Alex Pop <alexpop@users.noreply.github.com>

---
+++ libraries/apache.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# copyright: 2015, Vulcano Security GmbH
+
+class Apache < Inspec.resource(1)
+  name 'apache'
+  supports platform: 'unix'
+  desc 'Use the apache InSpec audit resource to retrieve Apache environment settings.'
+  example <<~EXAMPLE
+    describe apache do
+      its ('service') { should cmp 'apache2' }
+    end
+
+    describe apache do
+      its ('conf_dir') { should cmp '/etc/apache2' }
+    end
+
+    describe apache do
+      its ('conf_path') { should cmp '/etc/apache2/apache2.conf' }
+    end
+
+    describe apache do
+      its ('user') { should cmp 'www-data' }
+    end
+  EXAMPLE
+
+  attr_reader :service, :conf_dir, :conf_path, :user
+  def initialize
+    if inspec.os.debian?
+      @service = 'apache2'
+      @conf_dir = '/etc/apache2/'
+      @conf_path = File.join @conf_dir, 'apache2.conf'
+      @user = 'www-data'
+    else
+      @service = 'httpd'
+      @conf_dir = '/etc/httpd/'
+      @conf_path = File.join @conf_dir, '/conf/httpd.conf'
+      @user = 'apache'
+    end
+  end
+
+  def to_s
+    'Apache Environment'
+  end
+end
